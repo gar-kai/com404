9b3e4ea0f05da9f11b9685ba2993af78c26b8209
gar-kai <4wongg89@solent.ac.uk>
2019-10-01 09:35:59 +0000

    added code to demonstrate the use of an if...else statement.

---
+++ 1-basics/3-decision/2-if-else/bot.py
@@ -0,0 +1,8 @@
+#help Beep suggest activities to complete
+print("Please enter the activity to be performed")
+activity=input()
+if activity=="calculate":
+    print("Performing calculations...")
+else:
+    print("Perfoming activity...")
+print("Activity completed!")
