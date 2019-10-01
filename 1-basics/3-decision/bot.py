a4ec945fc7d0b073045b82f9a3247c9c8e9011b2
gar-kai <4wongg89@solent.ac.uk>
2019-10-01 09:10:21 +0000

    added code to demonstrate the use of an if statement

--- 1-basics/3-decision/bot.py
+++ 1-basics/3-decision/bot.py
@@ -1,7 +1,7 @@
 #ask what type of book Beep is reading
 print("What type of book do you like reading?")
 book=input()
-if book == ("Adventure"):
+if book == "Adventure":
     print("I like adventure books!")
 print("Finish reading book")
 
