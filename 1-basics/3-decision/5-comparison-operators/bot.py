cbb728c5130a528479d77c68db05c3cc8cd613a4
gar-kai <4wongg89@solent.ac.uk>
2019-10-01 10:34:42 +0000

    Added code to demonstrate the use of comparison operators

---
+++ 1-basics/3-decision/5-comparison-operators/bot.py
@@ -0,0 +1,11 @@
+#Beep is learning about mathematical operators
+print("Please enter the first number")
+number=(input())
+print("Please enter the second number")
+number2=(input())
+if(number<number2):
+    print("The first number is the smallest!")
+if(number2<number):
+    print("The second number is the smallest!")
+if(number==number2):
+    print("Both are equal!")
