73992f3b11376557fa146f520c8a7e6a02833207
gar-kai <4wongg89@solent.ac.uk>
2019-10-08 10:30:00 +0000

    Added code to demonstrate the use of a while look with len function.

---
+++ 1-basics/4-reptition/1-wile-loop/5-sum-100/bot.py
@@ -0,0 +1,10 @@
+#Bop calculate the sum of the first 100 numbers
+
+sum=0
+count= 0 
+while count !=101:
+    sum = sum + count
+    print(str(sum))
+    count = count +1
+
+print("Calculating the sum of the first 100 numbers...")
