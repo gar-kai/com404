84dc1797f7c66ee7cbaa2a3e72caa99ea5e81d59
gar-kai <4wongg89@solent.ac.uk>
2019-10-25 13:40:24 +0000

    TCA

---
+++ TCA/Question7/main.py
@@ -0,0 +1,17 @@
+def under(words):
+    print(word)
+    line = ""
+    for num in range(0,len(word)):
+        line = line + "*"
+    print(line)
+
+def over(word):
+    line = ""
+    for num in range(0, len(word)):
+        line = line + "*"
+    print(word)
+
+def both(word):
+    line = ""
+    for num in range(0,len(word)):
+        line = line + "*"
