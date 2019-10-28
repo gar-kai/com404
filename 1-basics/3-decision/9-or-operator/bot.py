96cbb826f56771c3daa1f952aae6181ef1eecf47
gar-kai <4wongg89@solent.ac.uk>
2019-10-28 10:12:59 +0000

    Added code to demonstrate the use of a logical or operator.

--- 1-basics/3-decision/9-or-operator/bot.py
+++ 1-basics/3-decision/9-or-operator/bot.py
@@ -1,15 +1,13 @@
 # Ask user for the type of adventure 
-print("What type of book is this?")
+print("What type of adventure should I have?")
 adventure_type = input() 
 
-# Identify the type of book
-if ((adventure_type == "scary") or (adventure_type=="short")):
-    print("\nEntering the dark forest")
-
-if ((adventure_type == "safe") or (adventure_type=="long")):
+# Determine what message to display
+if ( (adventure_type == "scary") or (adventure_type == "short") ):
+    print("\nEntering the dark forest!")
+elif ( (adventure_type == "safe") or (adventure_type == "long") ):
     print("\nTaking the safe route!")
-
 else:
-    print("Not sure which route to take")
+    print("\nNot sure which route to take.")
 
 
