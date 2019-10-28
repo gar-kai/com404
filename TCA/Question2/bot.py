4524a046badaff185ef74fe9e48964df713c2e70
gar-kai <4wongg89@solent.ac.uk>
2019-10-28 23:39:58 +0000

    TCA

--- TCA/Question2/bot.py
+++
@@ -1,7 +0,0 @@
-#Jedi apprentice
-print("Have you fear in your heart?")
-user =input()
-if user == "yes":
-    print("Fear is the path to the dark side. You cannot be a Jedi apprentice.")
-else: 
-    print("The force is strong in you. You may be a Jedi apprentice.")
