dbb76c16bbe2eab9962f6948101fc861e17192a7
gar-kai <4wongg89@solent.ac.uk>
2019-10-28 22:04:08 +0000

    TCA

--- TCA/Question3.3/bot.py
+++ TCA/Question3.3/bot.py
@@ -1,9 +1,8 @@
 # Ask user for number of cables
 print("How many miles must I travel before I reach the secret base?")
-cables_to_avoid = int(input())
+miles =int(input())
 
 
-
-for count in range(cables_to_avoid, 0, -1):
-    print("I have", cables_to_avoid, "to go before I reach the base.")
-    cables_to_avoid = cables_to_avoid -1
+for count in range(miles, 0, -1):
+    print("I have", miles, "to go before I reach the base.")
+    miles =miles -1
---
+++ TCA/Question4.1/bot.py
@@ -0,0 +1,9 @@
+def item_from_suitcase(item):
+    print("I wonder what is in my suitcase")
+
+
+
+#calls
+item_from_suitcase("toothbrush")
+item_from_suitcase("belt")
+item_from_suitcase("spidey suit")
