21ab74f268e6cbed84b90c9c89136da3b3b8e303
Gar-Kai Olivia Wong <4wongg89@solent.ac.uk>
2019-12-03 12:02:56 +0000

    Added code to images

--- 2-guis/4-images/gui.py
+++ 2-guis/4-images/gui.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import messagebox
 
 class Gui(Tk):
     
@@ -13,28 +14,45 @@ class Gui(Tk):
         self.plane_image= PhotoImage(file="//pclures06/home/4wongg89/Documents/Problem Solving/com404/2-guis/4-images/plane.gif")
 
         # add components
+        self.__add_main_frame()
         self.__add_heading_label()
         self.__add_ambulance_image_label()
         self.__add_bike_image_label()
         self.__add_plane_image_label()
 
+    def __add_main_frame(self):
+        self.main_frame=Frame()
+        self.main_frame.grid(row=0, column=0)
+
+
     def __add_heading_label(self):
-        self.heading_label = Label()
+        self.heading_label = Label(self.main_frame)
         self.heading_label.grid(row=0,column=0, columnspan=3)
         self.heading_label.configure(text="TRANSPORT")
         
     def __add_ambulance_image_label(self):
-        self.ambo_image_label = Label()
+        self.ambo_image_label = Label(self.main_frame)
         self.ambo_image_label.grid(row=1, column = 0)
         self.ambo_image_label.configure(image=self.ambo_image)
 
+
+    def __add_ambo_clicked(self, event):
+        self.ambo_clicked_label.bind("<button-1>", self.ambo_clicked)
+        textBoxValue=self.example_entry.get()
+        if textBoxValue == "":
+            textBoxValue = "Nothing"
+        messagebox.showinfo("Ambo" , "You have clicked the ambo and put." + textBoxValue + "in the text box")
+
     def __add_bike_image_label(self):
-        self.bike_image_label= Label()
+        self.bike_image_label= Label(self.main_frame)
         self.bike_image_label.grid(row=1, column =1)
         self.bike_image_label.configure(image=self.bike_image)
+
+    def __add_bike_clicked(self, event):
+        self.bike_clicked
  
     def __add_plane_image_label(self):
-        self.plane_image_label= Label()
+        self.plane_image_label= Label(self.main_frame)
         self.plane_image_label.grid(row=1, column=2)
         self.plane_image_label.configure(image=self.plane_image)
 
--- 2-guis/5-animation/1-single-object/gui.py
+++ 2-guis/5-animation/1-single-object/gui.py
@@ -7,7 +7,7 @@ class AnimatedGui(Tk):
         super().__init__()
         
         # load resources
-        self.ball_image = PhotoImage(file="U:/Documents/Problem Solving/com404/2-guis/5-animation/1-single-object/ambulance.gif")
+        self.ball_image = PhotoImage(file="//pclures06/home/4wongg89/Documents/Problem Solving/com404/2-guis/5-animation/1-single-object/ambulance.gif")
         
         # set window attributes
         self.configure(height=500,
@@ -16,6 +16,8 @@ class AnimatedGui(Tk):
         # set animation attributes
         self.ball_x_pos = 200
         self.ball_y_pos = 100
+        self.ball_x_change = 1
+        self.ball_y_change = 1
         
         # add components
         self.add_ball_image_label() 
@@ -25,23 +27,23 @@ class AnimatedGui(Tk):
         
     # the timer tick function    
     def tick(self):
-        self.after(100, self.tick)
+         self.after(100, self.tick)
 
-        if self.ball_y_pos > 450:
-            self.ball_y_change = -2
+            if self.ball_y_pos >450:
+                self.ball_y_change = -2
         
-        if self.ball_x_pos >450:
-            self.ball_x_change = -2
+            if self.ball_x_pos >450:
+                self.ball_x_change = -2
 
-        if self.ball_y_pos < 0:
-            self.ball_y_change = 2
+            if self.ball_y_pos < 0:
+                self.ball_y_change = 2
         
+ 
         self.ball_x_pos = self.ball_x_pos + self.ball_x_change
-        
         self.ball_y_pos = self.ball_y_pos + self.ball_y_change
         self.ball_image_label.place(x=self.ball_x_pos, 
                                     y=self.ball_y_pos)
-        self.after(50, self.tick)
+        self.after(100, self.tick)
 
     # the ball image
     def add_ball_image_label(self):
@@ -50,6 +52,8 @@ class AnimatedGui(Tk):
                                     y=self.ball_y_pos)
         self.ball_image_label.configure(image=self.ball_image)
      
+
+# the object
 if __name__ == "__main__":
     gui = AnimatedGui()    
     gui.mainloop() 
